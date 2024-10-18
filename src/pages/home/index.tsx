ddcf50298221eabae77d5b9385db8c61750f45a6
jameshsu1125 <jameshsu1125@gmail.com>
2024-10-18 14:11:47 +0800

    Update home page styles and add dynamic content rendering

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,17 +6,23 @@ const urls: { [key: string]: string } = {
   '1': 'https://30table.bitbucket.io/studio-template/2024-10-18__73d16/preview/',
   '0.5': 'https://30table.bitbucket.io/studio-template/2024-10-18__86a3e/preview/',
   '0': 'https://30table.bitbucket.io/studio-template/2024-10-18__1e556/preview/',
+  image: 'https://30table.bitbucket.io/studio-template/2024-10-18__ca0b7/preview/',
+  video: 'https://30table.bitbucket.io/studio-template/2024-10-18__57d46/preview/',
 };
 
 const Home = memo(() => {
-  useEffect(() => {}, []);
   const threshold = QueryString.get('threshold');
 
   return (
     <div className='Home'>
       <div id='parent-bg' />
-      <div className='flex h-[480px] w-full items-center justify-center bg-red-500'>
-        {threshold && urls[threshold] && <iframe src={urls[threshold]} width={320} height={480} />}
+      <div className='flex w-full items-center justify-center p-5'>
+        <div className='flex w-full justify-center bg-gray-100 py-5'>
+          {threshold && urls[threshold] && (
+            <iframe src={urls[threshold]} width={320} height={480} />
+            // <iframe src={'http://localhost:8081/'} width={320} height={480} />
+          )}
+        </div>
       </div>
       <div id='parent-bg' />
     </div>
